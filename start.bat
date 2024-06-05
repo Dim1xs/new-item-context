adc499de7cd99daa3c42205decac0275dc0d0462
Dim1xs <Dim1xs@yandex.ru>
2024-06-05 22:17:42 +0700

    Auto restart explorer

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 copy "%cd%\Templates\" "%appdata%\Microsoft\Windows\Templates\"
 regedit.exe /S item_format.reg
+echo "Explorer will be restarted!"
 TIMEOUT /T 10
+taskkill /f /im explorer.exe & start explorer.exe
